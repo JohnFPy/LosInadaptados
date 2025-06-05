a523208d097302770b6c6c8a5f03bab16f685136
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2025-06-05 13:04:01 -0500

    Create proyect

---
+++ Proyecto/Proyecto/MainClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    internal class MainClass
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello world.");
+            return 0;
+            // Detecta si el usuario esta logeado o no
+        }
+    }
+}
