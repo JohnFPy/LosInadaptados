51930ac6b22ae3ec330221ca8db4fc7055c2ffc3
trimsucne <secoes203@gmail.com>
2025-06-18 14:59:06 -0500

    calendar structure update

---
+++ Project/src/application/components/calendarView.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Project.application.components
+{
+    public class CalendarView
+    {
+        public ObservableCollection<DayView> Days { get; set; }
+        public string MonthYearText { get; set; }
+
+        public CalendarView()
+        {
+            var today = DateTime.Today;
+            MonthYearText = today.ToString("MMMM yyyy");
+
+            Days = new ObservableCollection<DayView>();
+
+            int daysInMonth = DateTime.DaysInMonth(today.Year, today.Month);
+            for (int i = 1; i <= daysInMonth; i++)
+            {
+                Days.Add(new DayView
+                {
+                    DayNumber = i.ToString(),
+                    EmotionColor = (i % 2 == 0) ? Brushes.LightPink : Brushes.LightBlue
+                });
+            }
+        }
+    }
+}
---
+++ Project/src/application/components/dayView.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Media;
+
+namespace Project.application.components
+{
+    public class DayView
+    {
+        public string DayNumber { get; set; }
+        public IBrush EmotionColor { get; set; }
+    }
+}
