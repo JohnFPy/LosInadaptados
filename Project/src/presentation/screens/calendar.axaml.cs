de33bb739f36f1bd541ae02c0fe294f6d863b08c
trimsucne <secoes203@gmail.com>
2025-06-18 15:12:05 -0500

    Update calendar.axaml.cs

--- Project/src/presentation/screens/calendar.axaml.cs
+++ Project/src/presentation/screens/calendar.axaml.cs
@@ -1,14 +1,16 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Project.application.components;
 
-namespace Project.presentation.components
+namespace Project.presentation.screens
 {
     public partial class calendar : UserControl
     {
         public calendar()
         {
             InitializeComponent();
+            DataContext = new CalendarView();
         }
 
         private void InitializeComponent()
